6f0fb263ab337634764f0963e674b8f010dcdf8f
mathias <mathiasdumaspro@gmail.com>
2024-04-09 20:31:06 +0200

    suite de l'application pokedex en react

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import React, {FunctionComponent, useState, useEffect} from 'react';
-import Pokemon from "./models/pokemon";
-import POKEMONS from "./models/mock-pokemon";
+import PokemonList from './pages/pokemon-list';
 
 
   
 const App: FunctionComponent  = () => {
- const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
-
-useEffect(() => {
-    setPokemons(POKEMONS);
-}, []);
 
     
  return (
-<div>
-    <h1 className='center'>Pokédex</h1>
-    <div className='container'>
-        <div className='row'>
-            {pokemons.map(({id, name, picture, created}) => (
-                <div className="col s6 m4" key={id}>
-                <div className='card horizontal'>
-                    <div className='card-image'>
-                        <img src={picture} alt={name}/>
-                    </div>
-                    <div className='card-stacker'>
-                        <div className='card-content'>
-                            <p>{name}</p>
-                            <p><small>{created.toString()}</small></p>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            ))}
-        </div>
-    </div>
-</div>
+<PokemonList/>
  )
 }
   
---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,88 @@
+import React, { FunctionComponent, useState } from 'react';
+import Pokemon from '../models/pokemon'
+import './pokemon-card.css';
+
+type Props = {
+    pokemon : Pokemon,
+    borderColor?: string
+};
+
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = "#009688"}) => {
+
+    const [color, setColor] = useState<string>();
+
+    const showBorder = () => {
+        setColor(borderColor);
+    }
+
+    const hideBorder = () => {
+        setColor("#f5f5f5");
+    }
+
+    const formatType = (type: string): string => {
+        let color: string;
+       
+        switch (type) {
+          case 'Feu': 
+            color = 'red lighten-1'; 
+            break; 
+          case 'Eau': 
+            color = 'blue lighten-1'; 
+            break; 
+          case 'Plante': 
+            color = 'green lighten-1'; 
+            break; 
+          case 'Insecte': 
+            color = 'brown lighten-1'; 
+            break; 
+          case 'Normal': 
+            color = 'grey lighten-3'; 
+            break; 
+          case 'Vol': 
+            color = 'blue lighten-3'; 
+            break; 
+          case 'Poison': 
+            color = 'deep-purple accent-1'; 
+            break; 
+          case 'Fée': 
+            color = 'pink lighten-4'; 
+            break; 
+          case 'Psy': 
+            color = 'deep-purple darken-2'; 
+            break; 
+          case 'Electrik': 
+            color = 'lime accent-1'; 
+            break; 
+          case 'Combat': 
+            color = 'deep-orange'; 
+            break; 
+          default: 
+            color = 'grey'; 
+            break; 
+        }
+       
+        return `chip ${color}`;
+    }
+    
+    return (
+        <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+            <div className="card horizontal" style={{borderColor: color}} >
+                <div className='card-image'>
+                    <img src={pokemon.picture} alt={pokemon.name}/>
+                </div>
+                <div className='card-stacked'>
+                    <div className='card-content'>
+                        <p>{pokemon.name}</p>
+                        <p><small>{formatDate(pokemon.created)}</small></p>
+                        {pokemon.types.map(type => (
+                            <span key={type} className={formatType(type)}>{type}</span>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PokemonCard;
+ 
